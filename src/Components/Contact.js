849419226426946ba45a845dd18eddcd86658b7d
Sarah Goewey <sarah.goewey@yahoo.com>
2023-08-20 11:45:06 -0400

    added contact links

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="container text-center">
       <h1>contact</h1>
+      <hr />
+      <div className="row">
+        <div className="col">
+          <a href="https://www.linkedin.com/in/sarah-goewey-software-engineer/">
+            LinkedIn
+          </a>
+        </div>
+        <div className="col">
+          <a href="https://github.com/sarah-goewey">GitHub</a>
+        </div>
+        <div className="col">
+          <a href="mailto:sarahgoewey627@gmail.com">Email</a>
+        </div>
+      </div>
     </div>
   );
 };
