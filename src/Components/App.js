ff61e5bb9e5ea50dea0d77f8cb5086ff6d2842bb
Sarah Goewey <sarah.goewey@yahoo.com>
2023-08-20 11:27:01 -0400

    created pages

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,18 +1,27 @@
 import React, { useEffect } from "react";
-import Home from "./Home";
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import Resume from "./Resume";
 import { Link, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <h1>Sarah Goewey</h1>
+      <h1>sarah goewey</h1>
 
       <nav>
-        <Link to="/">About</Link>
+        <Link to="/">about</Link>
+        <Link to="/resume">resume</Link>
+        <Link to="/projects">projects</Link>
+        <Link to="/contact">contact</Link>
       </nav>
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
