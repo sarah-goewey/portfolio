a7f110db8b9d5570d5c4b6af429b314876fc93cb
Sarah Goewey <sarah.goewey@yahoo.com>
2023-08-23 11:55:54 -0400

    reformatted projects page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,26 +8,38 @@ const Projects = () => {
         <h1>projects</h1>
         <hr />
         <div className="row">
+          <div className="col">Backup Cash Register</div>
+          <div className="col">About</div>
           <div className="col">
             <a href="https://backup-cash-register.onrender.com/#/">
-              Backup Cash Register
+              Deployed Site
             </a>
           </div>
-          <div className="col">
-            <a href="https://caves-of-mercury.onrender.com/">
-              Caves of Mercury
-            </a>
-          </div>
+          <div className="col">GitHub</div>
         </div>
         <div className="row">
+          <div className="col">Caves of Mercury</div>
+          <div className="col">About</div>
           <div className="col">
-            <a href="https://cooking-party.onrender.com/">Cooking Party</a>
+            <a href="https://caves-of-mercury.onrender.com/">Deployed Site</a>
           </div>
+          <div className="col">GitHub</div>
+        </div>
+        <div className="row">
+          <div className="col">Cooking Party</div>
+          <div className="col">About</div>
           <div className="col">
-            <a href="https://spiffy-stickies.onrender.com/#/">
-              Spiffy Stickies
-            </a>
+            <a href="https://cooking-party.onrender.com/">Deployed Site</a>
           </div>
+          <div className="col">GitHub</div>
+        </div>
+        <div className="row">
+          <div className="col">Spiffy Stickies</div>
+          <div className="col">About</div>
+          <div className="col">
+            <a href="https://spiffy-stickies.onrender.com/#/">Deployed Site</a>
+          </div>
+          <div className="col">GitHub</div>
         </div>
       </div>
     </div>
